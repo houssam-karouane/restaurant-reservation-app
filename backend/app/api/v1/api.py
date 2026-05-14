9ffc181b80b47cc99eec7c99fe0183c115a6025c
unknown <medelmami478@gmail.com>
2026-05-14 15:35:21 +0100

    feat(recommendations): add Pydantic schemas RecommendationItem Response

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, reservations, users
+from app.api.v1.endpoints import auth, reservations, users, recommendations
 from app.api.v1.endpoints import restaurants
 
 api_router = APIRouter()
@@ -7,3 +7,4 @@ api_router.include_router(auth.router, prefix="/auth", tags=["Authentification"]
 api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(restaurants.router, prefix="/restaurants", tags=["Restaurants"])
 api_router.include_router(reservations.router, prefix="/reservations", tags=["reservations"])
+api_router.include_router(recommendations.router, prefix="/recommendations", tags=["Recommendations"])
